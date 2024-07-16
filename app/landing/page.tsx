c427296595648bcc9fb996b1a74cada0c9f5a2b9
aljodavis <aljodavis125@gmail.com>
2024-07-16 11:11:17 +0530

    fix: set card mapping

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import { PiCheckCircleThin } from "react-icons/pi";
 import { PiCaretRightThin } from "react-icons/pi";
@@ -6,11 +5,34 @@ import { Button } from "@/components/ui/button";
 import React from 'react'
 
 const landing = () => {
+    const data = [
+        {
+            title: "Effortless Task Creation",
+            text: "Seamlessly create new tasks with ease, whether you're working individually or collaborating with your team.",
+            img: "/images/work1.jpg"
+        },
+        {
+            title: "Organization with Streamlined List View",
+            text: "Immerse yourself in a clean and intuitive list view that enhances visibility and simplifies your workflow.",
+            img: "/images/work2.jpg"
+        },
+        {
+            title: "Potential of Your Efficiency with AutoFlow",
+            text: "Seamlessly automate repetitive tasks, streamline processes, and elevate your productivity to new heights.",
+            img: "/images/work3.jpg"
+        },
+        {
+            title: "Summaries for Enhanced Task Management",
+            text: "Elevate your productivity with succinct task summaries that provide a quick overview of key details.",
+            img: "/images/work4.jpg"
+        }
+    ];
+
     return (
         <main className="bg-white font-Switzer w-full h-full">
             <section className="bg-white w-full h-full">
                 <div className="flex flex-col md:flex-row mt-20 mx-5 md:mx-20 justify-between items-center gap-10">
-                    <h1 className="text-2xl md:text-4xl ml-0 md:ml-20 font-bold w-full md:w-[45%] text-center md:text-left">
+                    <h1 className="text-2xl md:text-4xl mt-5 ml-0 md:ml-20 font-bold w-full md:w-[45%] text-center md:text-left">
                         Discover How Various Departments Use Taskmanly.
                     </h1>
                     <p className="text-sm text-slate-400 w-full md:w-[30%] leading-relaxed text-center md:text-left mr-0 md:mr-36">
@@ -18,21 +40,21 @@ const landing = () => {
                     </p>
                 </div>
                 <div className="mt-14 mx-5 md:mx-40 flex flex-wrap justify-center gap-3 md:gap-6 lg:gap-16">
-                    <div className="border rounded-md border-black font-Switzer font-medium bg-[#F5F5F5] text-black px-6 md:px-11 py-2">
+                    <Button className="border rounded-md border-black font-Switzer font-medium bg-[#F5F5F5] text-black px-6 md:px-11 py-2">
                         Marketing
-                    </div>
-                    <div className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-6 md:px-11 py-2">
+                    </Button>
+                    <Button className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-6 md:px-11 py-2">
                         Operations
-                    </div>
-                    <div className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-10 md:px-20 py-2">
+                    </Button>
+                    <Button className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-10 md:px-20 py-2">
                         IT
-                    </div>
-                    <div className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-10 md:px-14 py-2">
+                    </Button>
+                    <Button className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-10 md:px-14 py-2">
                         Product
-                    </div>
-                    <div className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-6 md:px-11 py-2">
+                    </Button>
+                    <Button className="border rounded-md bg-[#F5F5F5] font-Switzer font-medium text-black px-6 md:px-11 py-2">
                         Company-wide
-                    </div>
+                    </Button>
                 </div>
                 <div className="mt-14 mb-14 mx-5 md:mx-40 flex flex-col md:flex-row gap-10 md:gap-28 justify-center items-center">
                     <Image
@@ -66,8 +88,7 @@ const landing = () => {
                 </div>
             </section>
 
-
-            <section className="bg-black text-white w-full h-full mt-5">
+            <section className="bg-black pb-10 text-white w-full h-full mt-5">
                 <div className="mt-20 p-5 md:p-14 mx-5 md:mx-20 font-Switzer">
                     <h1 className="text-2xl md:text-4xl font-bold text-center mb-4">
                         Focus on Tasks that Generate Revenue.
@@ -77,83 +98,31 @@ const landing = () => {
                     </p>
                 </div>
 
-                <div className="flex flex-col md:flex-row justify-center mx-auto md:space-x-6 space-y-6 md:space-y-0">
-                    <div className="rounded-md border border-white p-2 w-full md:w-[40%] overflow-hidden">
-                        <h1 className="text-xl font-bold mb-2">Effortless Task Creation</h1>
-                        <p className="text-sm mb-4 text-[#C2C2C2]">
-                            Seamlessly create new tasks with ease, whether you're working individually or collaborating with your team.
-                        </p>
-                        <div className="flex items-center text-sm mb-4">
-                            <p className="mr-2">Learn more</p>
-                            <PiCaretRightThin />
+                <div className="grid grid-cols-1  md:grid-cols-2 gap-7 mx-16 px-3 md:px-12 lg:px-24">
+                    {data.map((item, index) => (
+                        <div key={index} className="rounded-md border border-white p-4 w-full md:w-[80%] lg:w-[90%] overflow-hidden space-y-3">
+                            <h1 className="text-xl font-bold">{item.title}</h1>
+                            <p className="text-sm text-[#C2C2C2]">{item.text}</p>
+                            <div className="flex items-center text-sm">
+                                <p className="mr-2">Learn more</p>
+                                <PiCaretRightThin />
+                            </div>
+                            <Image
+                                src={item.img}
+                                width={850}
+                                height={600}
+                                alt="Picture of the bg"
+                                className="rounded-sm object-cover w-full"
+                            />
                         </div>
-                        <Image
-                            src="/images/work1.jpg"
-                            width={800}
-                            height={550}
-                            alt="Picture of the bg"
-                            className="rounded-sm object-cover w-full"
-                        />
-                    </div>
-                    <div className="rounded-md border border-white p-2 w-full md:w-[40%] overflow-hidden">
-                        <h1 className="text-xl font-bold mb-2">Organization with Streamlined List View</h1>
-                        <p className="text-sm mb-4 text-[#C2C2C2]">
-                            Immerse yourself in a clean and intuitive list view that enhances visibility and simplifies your workflow.
-                        </p>
-                        <div className="flex items-center text-sm mb-4">
-                            <p className="mr-2">Learn more</p>
-                            <PiCaretRightThin />
-                        </div>
-                        <Image
-                            src="/images/work2.jpg"
-                            width={800}
-                            height={600}
-                            alt="Picture of the bg"
-                            className="rounded-sm object-cover w-full"
-                        />
-                    </div>
+                    ))}
                 </div>
 
-                <div className="mt-6 py-1 flex flex-col md:flex-row justify-center mx-auto md:space-x-6 space-y-6 md:space-y-0">
-                    <div className="rounded-md border border-white p-2 w-full md:w-[40%] overflow-hidden">
-                        <h1 className="text-xl font-bold mb-2">Potential of Your Efficiency with AutoFlow</h1>
-                        <p className="text-sm mb-4 text-[#C2C2C2]">
-                            Seamlessly automate repetitive tasks, streamline processes, and elevate your productivity to new heights.
-                        </p>
-                        <div className="flex items-center text-sm mb-4">
-                            <p className="mr-2">Learn more</p>
-                            <PiCaretRightThin />
-                        </div>
-                        <Image
-                            src="/images/work3.jpg"
-                            width={800}
-                            height={550}
-                            alt="Picture of the bg"
-                            className="rounded-sm object-cover w-full"
-                        />
-                    </div>
-                    <div className="rounded-md border border-white p-2 w-full md:w-[40%] overflow-hidden">
-                        <h1 className="text-xl font-bold mb-2">Summaries for Enhanced Task Management</h1>
-                        <p className="text-sm mb-4 text-[#C2C2C2]">
-                            Elevate your productivity with succinct task summaries that provide a quick overview of key details.
-                        </p>
-                        <div className="flex items-center text-sm mb-4">
-                            <p className="mr-2">Learn more</p>
-                            <PiCaretRightThin />
-                        </div>
-                        <Image
-                            src="/images/work4.jpg"
-                            width={800}
-                            height={600}
-                            alt="Picture of the bg"
-                            className="rounded-sm object-cover w-full"
-                        />
-                    </div>
-                </div>
+
+
             </section>
-
         </main>
     )
 }
 
-export default landing
+export default landing;
